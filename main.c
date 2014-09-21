cdfd45f24f8d184c7cd8ce8e031add14b205d801
Joao Sallaberry <joaovictorhs@gmail.com>
2014-09-21 19:35:05 -0300

    WIP: lexer treating float..

--- main.c
+++ main.c
@@ -3,15 +3,15 @@
 #include "token.h"
 
 // define states
-typedef enum {ST_INITIAL, ST_NUMBER, ST_ALPHANUM, ST_SYMBOL, NUM_STATES} state_t;
-typedef enum {EN_DIGIT, EN_ALPHA, EN_SYMBOL, EN_SPACE, NUM_ENTRIES} entry_type_t;
+typedef enum {ST_INITIAL, ST_NUMBER, ST_FLOAT, ST_ALPHANUM, ST_SYMBOL, NUM_STATES} state_t;
+typedef enum {EN_DIGIT, EN_ALPHA, EN_SYMBOL, EN_DOT, EN_SPACE, NUM_ENTRIES} entry_type_t;
 
 state_t const next_state[][NUM_ENTRIES] = {
-// receive        digit         alpha         symbol        space 
-/* INITIAL */    {ST_NUMBER,   ST_ALPHANUM,   ST_SYMBOL,    ST_INITIAL},
-/* NUMBER */     {ST_NUMBER,   0,             0,            ST_INITIAL},
-/* ALPHANUM */   {ST_ALPHANUM, ST_ALPHANUM,   0,            ST_INITIAL},
-/* SYMBOL */     {0,           0,             ST_SYMBOL,    ST_INITIAL}
+// receive        digit         alpha         symbol        dot           space 
+/* INITIAL */    {ST_NUMBER,   ST_ALPHANUM,   ST_SYMBOL,    0,            ST_INITIAL},
+/* NUMBER */     {ST_NUMBER,   0,             0,            ST_FLOAT,     ST_INITIAL},
+/* ALPHANUM */   {ST_ALPHANUM, ST_ALPHANUM,   0,            0,            ST_INITIAL},
+/* SYMBOL */     {0,           0,             ST_SYMBOL,    0,            ST_INITIAL}
 };
 
 
@@ -21,7 +21,10 @@ void do_initial_digit();
 void do_initial_alpha();
 void do_initial_symbol();
 void do_number_digit();
+void do_number_dot();
 void do_number_space();
+void do_float_digit();
+void do_float_space();
 void do_alphanum_digit();
 void do_alphanum_alpha();
 void do_alphanum_space();
@@ -29,11 +32,12 @@ void do_symbol_symbol();
 void do_symbol_space();
 
 void (*const action_table [NUM_STATES][NUM_ENTRIES]) (void) = {
-//                digit                alpha                symbol                 space
-/* INITIAL */    {do_initial_digit,    do_initial_alpha,    do_initial_symbol,     do_nothing},
-/* NUMBER */     {do_number_digit,     do_nothing,          do_nothing,            do_number_space},
-/* ALPHANUM */   {do_alphanum_digit,   do_alphanum_alpha,   do_nothing,            do_alphanum_space},
-/* SYMBOL */     {do_nothing,          do_nothing,          do_symbol_symbol,      do_symbol_space}
+//                digit                alpha                dot             symbol                 space
+/* INITIAL */    {do_initial_digit,    do_initial_alpha,    do_nothing,     do_initial_symbol,     do_nothing},
+/* NUMBER */     {do_number_digit,     do_nothing,          do_nothing,     do_nothing,            do_number_space},
+/* FLOAT */      {do_float_digit,      do_nothing,          do_nothing,     do_nothing,            do_float_space},
+/* ALPHANUM */   {do_alphanum_digit,   do_alphanum_alpha,   do_nothing,     do_nothing,            do_alphanum_space},
+/* SYMBOL */     {do_nothing,          do_nothing,          do_nothing,     do_symbol_symbol,      do_symbol_space}
 };
 
 
@@ -57,6 +61,8 @@ entry_type_t classify_entry(char c) {
 	return EN_ALPHA;
     else if (is_symbol(c))
 	return EN_SYMBOL;
+    else if (c == '.')
+	return EN_DOT;
     else
 	fprintf(stderr, "entry char is of unknown type\n");
     return 1;
@@ -66,6 +72,7 @@ entry_type_t classify_entry(char c) {
 char buffer[256];
 size_t buffer_pt = 0;
 int buffer_int;
+float buffer_fl;
 char buffer_sym;
 token_type_t token_type;
 
@@ -123,6 +130,14 @@ void do_number_digit() {
 
 void do_number_space() {
     add_int_token(buffer_int);
+}
+
+void do_float_digit() {
+    buffer_fl +=
+}
+
+void do_float_space() {
+    add_float_token(buffer_int + buffer_float);
 }
 
 void do_alphanum_digit() {
